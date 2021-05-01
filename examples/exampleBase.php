cc8fa17654c260affe66dc718fa0d077163b9a1c
Tim Vögtli <tim.voegtli@hf-ict.info>
2021-05-01 14:33:50 +0200

    removed debug from exaple

--- examples/exampleBase.php
+++ examples/exampleBase.php
@@ -1,11 +1,6 @@
 <?php
 require '../vendor/autoload.php';
 
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(E_ALL);
-
-
 use Webchaind\Webchaind;
 use Webchaind\ERC20;
 use Webchaind\Account;
